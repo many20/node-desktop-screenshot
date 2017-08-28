39fbe5e213f29900d22649120afed1131ad5aa42
Mario <many.mario@gmx.de>
2017-08-28 12:00:06 +0200

    added multi monitor options to win32 and linux

--- capture/win32.js
+++ capture/win32.js
@@ -3,8 +3,13 @@ module.exports = function(options, callback) {
 	var fs = require('fs');
 	var childProcess = require('child_process');
 	var path = require('path');
-	
-	var nircmd = childProcess.spawn(path.join(__dirname, "bin", "nircmd.exe"), ["savescreenshot", options.output]);	
+
+	var args = ["savescreenshot", options.output];
+    if (!!options.multiScreen) {
+        args = ["savescreenshotfull", options.output];
+	}
+
+	var nircmd = childProcess.spawn(path.join(__dirname, "bin", "nircmd.exe"), args);
 
 	nircmd.on('close', function(code, signal) {
 		try {
